4b0804185c2a46006c7221666397d368571d20a2
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-16 00:02:44 +0530

    Create ClientApp_Autowiring_byName.java

---
+++ com/app/test/ClientApp_Autowiring_byName.java
@@ -0,0 +1,17 @@
+package com.app.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.beans.Employee;
+
+public class ClientApp_Autowiring_byName {
+
+	public static void main(String[] args) {
+		//read and print the employee i.e parent object
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("com/app/cfg/Springconfig_Autowiring_byName.xml");
+		Employee e=(Employee)ctx.getBean("emp");
+			System.out.println(e);
+	}
+
+}
